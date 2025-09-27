cd571487758468712cf9b12a7aca99d43b603568
sharmamanikant <145469832+sharmamanikant@users.noreply.github.com>
2025-09-28 01:56:05 +0530

    Update proxmox.js

--- mycloud-platform/backend/proxmox.js
+++ mycloud-platform/backend/proxmox.js
@@ -3,6 +3,6 @@ const axios = require("axios");
 const client = axios.create({
   baseURL: process.env.https://10.30.0.100:8006/#v1:0:18:4:5::::::=apitokens,
   headers: {
-    Authorization: `PVEAPIToken=${process.env.PROXMOX_USER}!${process.env.root@pam!cloud}=${process.env.e9418a2d-e7ea-43fa-95d2-2cbfdb9a32b8}`,
+    Authorization: `PVEAPIToken=${process.env.root}!${process.env.root@pam!cloud}=${process.env.e9418a2d-e7ea-43fa-95d2-2cbfdb9a32b8}`,
   },
 });
