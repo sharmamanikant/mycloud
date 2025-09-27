78343da0fb99333956ea30028a440b3b12131ea3
sharmamanikant <145469832+sharmamanikant@users.noreply.github.com>
2025-09-28 02:03:16 +0530

    Update auth.js

--- mycloud-platform/backend/src/middleware/auth.js
+++ mycloud-platform/backend/src/middleware/auth.js
@@ -5,10 +5,11 @@ module.exports = function (req, res, next) {
   if (!token) return res.status(401).json({ msg: "No token, authorization denied" });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.b4ff2064b0e1fd12be472e3b2e05d546c4225839b07544fe7d7d7a53f87403acae0ee50bdbc8c7875eca93b9907668463d686153a9f17bcc142285098677a4a4);
     req.user = decoded;
     next();
   } catch (err) {
     res.status(401).json({ msg: "Token is not valid" });
   }
 };
+
