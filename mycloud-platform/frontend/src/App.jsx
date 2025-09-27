d9788c8cc2b3c096993a52b9b81b45e9559f71e0
sharmamanikant <145469832+sharmamanikant@users.noreply.github.com>
2025-09-28 01:20:43 +0530

    Update App.jsx

--- mycloud-platform/frontend/src/App.jsx
+++ mycloud-platform/frontend/src/App.jsx
@@ -1,35 +1,3 @@
-// src/App.jsx
-import React, { useState, useEffect } from 'react';
-import Dashboard from './components/Dashboard';
-import CreateVMForm from './components/CreateVMForm';
-import Login from './components/Login';
-
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) setIsLoggedIn(true);
-  }, []);
-
-  return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <header className="text-2xl font-bold mb-4 text-center text-blue-700">
-        ☁️ MyCloud Platform
-      </header>
-
-      {!isLoggedIn ? (
-        <Login onLoginSuccess={() => setIsLoggedIn(true)} />
-      ) : (
-        <>
-          <Dashboard />
-          <CreateVMForm />
-        </>
-      )}
-    </div>
-  );
-}
-
 export default App;
 import { useState } from "react";
 import Login from "./components/Login";
@@ -46,3 +14,35 @@ function App() {
 }
 
 export default App;
+
+#// src/App.jsx
+#import React, { useState, useEffect } from 'react';
+#import Dashboard from './components/Dashboard';
+#import CreateVMForm from './components/CreateVMForm';
+#import Login from './components/Login';
+
+#function App() {
+#  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+#  useEffect(() => {
+ #   const token = localStorage.getItem('token');
+ #   if (token) setIsLoggedIn(true);
+  #}, []);
+
+ # return (
+ #   <div className="min-h-screen bg-gray-100 p-4">
+  #    <header className="text-2xl font-bold mb-4 text-center text-blue-700">
+  #      ☁️ MyCloud Platform
+  #    </header>
+
+  #   {!isLoggedIn ? (
+  #      <Login onLoginSuccess={() => setIsLoggedIn(true)} />
+  #    ) : (
+  #      <>
+   #       <Dashboard />
+   #       <CreateVMForm />
+   #     </>
+    #  )}
+    #</div>
+ # );
+#}
